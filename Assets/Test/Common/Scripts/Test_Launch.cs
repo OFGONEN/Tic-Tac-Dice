1970b32c195ca61d68f56adffb64591c444098e0
ofgonen <ofgonen@gmail.com>
2021-07-27 17:44:38 +0300

    Test #Detecting which side of the dice is facing up
    - Calculating Dot Product of every angle vector of dice, then selecting a result with 1 or -1

--- Assets/Test/Common/Scripts/Test_Launch.cs
+++ Assets/Test/Common/Scripts/Test_Launch.cs
@@ -2,6 +2,8 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using FFStudio;
 using NaughtyAttributes;
 using UnityEditor;
 using UnityEngine;
@@ -15,6 +17,8 @@ public class Test_Launch : MonoBehaviour
 
 	private Vector3 start_Position;
 	private Quaternion start_Rotation;
+
+	private UnityMessage fixedUpdateDelegate;
 #endregion
 
 #region Unity API
@@ -27,6 +31,13 @@ public class Test_Launch : MonoBehaviour
 
 		start_Position = transform.position;
 		start_Rotation = transform.rotation;
+
+		fixedUpdateDelegate = ExtensionMethods.EmptyMethod;
+	}
+
+	private void FixedUpdate() 
+	{
+		fixedUpdateDelegate();
 	}
 #endregion
 
@@ -48,6 +59,34 @@ public class Test_Launch : MonoBehaviour
 
 		dice_rigidbody.AddForce( throwVector, ForceMode.Impulse );
 		dice_rigidbody.AddTorque( Random.insideUnitSphere, ForceMode.Impulse );
+
+		DOVirtual.DelayedCall( 1f, () => fixedUpdateDelegate = VelocityCheck );
+		fixedUpdateDelegate = VelocityCheck;
+	}
+
+	private void VelocityCheck()
+	{
+		if( dice_rigidbody.IsSleeping() )
+		{
+			var dotProduct_up     = Vector3.Dot( transform.up, Vector3.up );
+			var dotProduct_foward = Vector3.Dot( transform.forward, Vector3.up );
+			var dotProduct_right  = Vector3.Dot( transform.right, Vector3.up );
+
+			var facing_up     = Mathf.Approximately( 1, Mathf.Abs( dotProduct_up ) );
+			var facing_foward = Mathf.Approximately( 1, Mathf.Abs( dotProduct_foward ) );
+			var facing_right  = Mathf.Approximately( 1, Mathf.Abs( dotProduct_right ) );
+
+			if( facing_up || facing_foward || facing_right )
+			{
+				FFLogger.Log( $"Facing up:{facing_up} - Facing foward:{facing_foward} - Facing right:{facing_right}" );
+				fixedUpdateDelegate = ExtensionMethods.EmptyMethod;
+			}
+			else
+			{
+				FFLogger.Log( "Facing Angular" );
+				dice_rigidbody.AddForce( Vector3.up * 3, ForceMode.Impulse );
+			}
+		}
 	}
 #endregion
 
@@ -59,6 +98,18 @@ public class Test_Launch : MonoBehaviour
 		var targetPosition = ( transform.position + throwVector.normalized * 3) ;
 		Handles.DrawLine( transform.position, targetPosition );
 	}
+
+	[ Button() ]
+	private void LogDotProduct()
+	{
+		var dotProduct_up     = Vector3.Dot( transform.up, Vector3.up );
+		var dotProduct_foward = Vector3.Dot( transform.forward, Vector3.up );
+		var dotProduct_right  = Vector3.Dot( transform.right, Vector3.up );
+
+		FFLogger.Log( "Up: " + dotProduct_up );
+		FFLogger.Log( "Forward: " + dotProduct_foward );
+		FFLogger.Log( "Right: " + dotProduct_right );
+}
 #endif
 #endregion
 }
