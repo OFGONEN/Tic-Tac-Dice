ba8de3b2f057935c81d686c23f8a4ac546da47fd
ofgonen <ofgonen@gmail.com>
2021-08-12 04:12:57 +0300

    Soldier: Implement attack related methods

--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
+using DG.Tweening;
 using NaughtyAttributes;
 using UnityEditor;
 
@@ -17,14 +18,19 @@ public class Soldier : MonoBehaviour
 	[ HorizontalLine, Header( "Setup" ) ]
 	[ SerializeField ] private SoldierData soldierData;
 
+	// Public Properties \\
+	public SoldierData SoldierData => soldierData;
+	public bool IsSoldierDead => !alive;
 
 	// Private Fields \\
 	private Animator animator;
 
 	// Attack Releated
 	private Soldier attackTarget;
-	private Vector3 attackPoint;
-	private int currentHealth;
+	[ SerializeField, ReadOnly ] private int currentHealth;
+	private bool alive = false;
+	private bool canAttack = true;
+	private Tween attackCooldownTween;
 
 	// Delegate
 	private UnityMessage updateMethod;
@@ -41,6 +47,11 @@ public class Soldier : MonoBehaviour
 		currentHealth = soldierData.health;
 	}
 
+	private void OnDisable()
+	{
+		KillTweens();
+	}
+
 	private void Update()
 	{
 		updateMethod();
@@ -51,33 +62,57 @@ public class Soldier : MonoBehaviour
 	// Spawns the soldier with given world position and rotation.
 	public void Spawn( Vector3 position, Quaternion rotation )
 	{
+		// Set default values
+		currentHealth = soldierData.health;
+		alive         = true;
+		canAttack     = true;
+
 		gameObject.SetActive( true );
 
 		transform.position = position;
 		transform.rotation = rotation;
 	}
 
+	// Attack a target soldier
 	public void Attack( Soldier target )
 	{
-		attackTarget = target;
-		attackPoint  = ( target.transform.position + transform.position ) / 2f;
+		attackTarget = target; // Cache the attack target
 
 		animator.SetBool( "running", true );
 
-		updateMethod = AttackUpdate;
+		updateMethod = OnAttackUpdate; // Move towards attack target every frame, attack it if possible.
+	}
+
+	// Take damage from attacking soldier. If our attack is same soldier as the attacker , attack as counter attack
+	public void TakeDamage( Soldier attacker, bool counterAttack ) // Counter attack bool is there for preventing this method called indefinitly as recursive
+	{
+		currentHealth--;
+
+		if( currentHealth <= 0 )
+		{
+			// If attacker is the same soldier we want to attack, we should attack at the same frame as counter attack.
+			if( attackTarget != null && attackTarget == attacker && !counterAttack ) 
+				attackTarget.TakeDamage( this, true ); // Attack as counter attack
+
+			Die();
+		}
 	}
 #endregion
 
 #region Implementation
-	private void AttackUpdate()
+	private void OnAttackUpdate()
 	{
-		// Move towards attack point
-		transform.position = Vector3.MoveTowards( transform.position, attackPoint, Time.deltaTime * soldierData.speed_Running );
+		var targetPosition = attackTarget.transform.position;
 
-		// Look at towards attack point 
-		transform.LookAtOverTimeAxis( attackPoint, Vector3.up, Time.deltaTime * soldierData.speed_Rotation );
+		// Target soldier died before we attacked it.
+		if( attackTarget.IsSoldierDead )
+		{
+			updateMethod = ExtensionMethods.EmptyMethod;
 
-		if( Vector3.Distance( transform.position, attackPoint ) <= soldierData.radius)
+			animator.SetBool( "running", false );
+		}
+		// Target is in attack range, attack it.
+		else if( canAttack && Vector3.Distance( transform.position, targetPosition ) <= ( soldierData.radius + attackTarget.SoldierData.radius ) )
 		{
 			updateMethod = ExtensionMethods.EmptyMethod;
 
@@ -85,15 +120,58 @@ public class Soldier : MonoBehaviour
 			animator.SetBool( "running", false );
 
 			// Attack
+			attackTarget.TakeDamage( this, false );
+
+			canAttack = false;
+
+			if( attackTarget.IsSoldierDead )
+				attackCooldownTween = DOVirtual.DelayedCall( soldierData.attackCooldown, OnAttackCoooldownFinish );
+			else
+				attackCooldownTween = DOVirtual.DelayedCall( soldierData.attackCooldown, OnAttackRepeat );
 		}
+		else // Move and rotates towards attack target.
+		{
+			// Move towards attack target.
+			transform.position = Vector3.MoveTowards( transform.position, targetPosition, Time.deltaTime * soldierData.speed_Running );
+
+			// Look at towards attack target. 
+			transform.LookAtOverTimeAxis( targetPosition, Vector3.up, Time.deltaTime * soldierData.speed_Rotation );
+		}
+	}
+
+	private void OnAttackCoooldownFinish()
+	{
+		attackCooldownTween = null;
+		canAttack = true;
+	}
+	
+	private void OnAttackRepeat()
+	{
+		canAttack = true;
+		OnAttackUpdate();
+	}
+
+	private void Die()
+	{
+		alive     = false;
+		canAttack = false;
+
+		ReturnToDefault();
 	}
 
 	private void ReturnToDefault()
 	{
-		currentHealth = soldierData.health;
-
 		gameObject.SetActive( false );
 		soldierPool.Stack.Push( this );
+	}
+
+	private void KillTweens()
+	{
+		if( attackCooldownTween != null )
+		{
+			attackCooldownTween.Kill();
+			attackCooldownTween = null;
+		}
 	}
 #endregion
 
