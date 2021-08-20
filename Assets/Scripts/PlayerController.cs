b2ee37c1c0f28853855c7b9f060d0ba3a5efa020
ofgonen <ofgonen@gmail.com>
2021-08-20 13:25:21 +0300

    PlayerController: Enables & disables target gameobject
    - Enables target gameobject when targeting, disables after launch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -45,6 +45,8 @@ public class PlayerController : MonoBehaviour
     {
 		levelStartListener.response  = LevelStartResponse;
 		levelFinishListener.response = LevelFinishResponse;
+
+		target.gameObject.SetActive( false );
 	}
 
 #endregion
@@ -55,12 +57,12 @@ public class PlayerController : MonoBehaviour
 #region Implementation
     private void LevelStartResponse()
     {
-		inputActiveProperty.changeEvet     += InputActiveResponse;
+		inputActiveProperty.changeEvet += InputActiveResponse;
 	}
 
     private void LevelFinishResponse()
     {
-		inputActiveProperty.changeEvet     -= InputActiveResponse;
+		inputActiveProperty.changeEvet -= InputActiveResponse;
 
 		targeting = false;
 	}
@@ -75,13 +77,15 @@ public class PlayerController : MonoBehaviour
 			diceThrower.UpdateTargetPoint( diceThrower.ClosestTargetPosition );
 
 			targeting = true;
-            inputDirectionProperty.changeEvent += InputDirectionChangeResponse;
+			target.gameObject.SetActive( true );
+			inputDirectionProperty.changeEvent += InputDirectionChangeResponse;
 		}
         else if ( targeting && !inputActiveProperty.sharedValue )
         {
 			diceThrower.Launch();
 
 			targeting = false;
+			target.gameObject.SetActive( false );
             inputDirectionProperty.changeEvent -= InputDirectionChangeResponse;
 		}
     }
